ab097fb1f19278530bdaa82948aecb4ed5e0a1a1
ianverger <ian.verger@gmail.com>
2022-11-03 14:52:17 -0400

    done in-person

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -1,7 +1,6 @@
 import LoginForm from "./SessionForms/LoginForm";
 import SignupFormModal from "./SessionForms/SignupModal";
 import './MainPage.css';
-import CarouselUnit from "./Carousel";
 import Carousel from 'react-bootstrap/Carousel';
 import DemoUserButton from "./SessionForms/DemoUser";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -28,13 +27,6 @@ function MainPage() {
             <Carousel.Item>
               <img
                 className="d-block w-100"
-                src="https://hippark-photos.s3.amazonaws.com/allora-pics/boxed-water-is-better--mLNxpdLRBk-unsplash.jpg"
-                alt="First slide"
-              />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
                 src="https://hippark-photos.s3.amazonaws.com/allora-pics/nicole-herrero-rWWLpxSefp8-unsplash.jpg"
                 alt="Second slide"
               />
@@ -42,8 +34,50 @@ function MainPage() {
             <Carousel.Item>
               <img
                 className="d-block w-100"
-                src="https://hippark-photos.s3.amazonaws.com/allora-pics/tron-le-JsuBKjHGDMM-unsplash.jpg"
+                src="https://hippark-photos.s3.amazonaws.com/allora-pics/boxed-water-is-better--mLNxpdLRBk-unsplash.jpg"
                 alt="Third slide"
+              />
+            </Carousel.Item>
+          <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://images.pexels.com/photos/2325446/pexels-photo-2325446.jpeg"
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://images.pexels.com/photos/4825701/pexels-photo-4825701.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt="Fourth slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://images.pexels.com/photos/4356144/pexels-photo-4356144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt="Sixth slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://images.pexels.com/photos/1000445/pexels-photo-1000445.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt="Seventh slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://images.pexels.com/photos/716421/pexels-photo-716421.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt="Eighth slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://hippark-photos.s3.amazonaws.com/allora-pics/tron-le-JsuBKjHGDMM-unsplash.jpg"
+                alt="Ninth slide"
               />
             </Carousel.Item>
           </Carousel>
