e126aefe3546effecd2c59ee0baa6b241f7f34fb
heatherpark201 <heatherpark201@gmail.com>
2022-11-22 17:00:40 -0500

    update NewTripForm

--- frontend/src/components/TripShow/TripMap.js
+++ frontend/src/components/TripShow/TripMap.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import { useHistory } from 'react-router-dom';
-import Geocode from "react-geocode";
-import ActivitiesMap from '../Map/Map'
-Geocode.setApiKey(process.env.REACT_APP_MAPS_API_KEY);
+import ActivitiesMap from '../Map/Map';
 
 function TripMap ({city, activities}) {
     const history = useHistory();
@@ -10,74 +8,18 @@ function TripMap ({city, activities}) {
     const [mapLoaded, setMapLoaded] = useState(false);
     const [highlightedActivity,setHighlightedActivity] = useState(null);
     const [bounds, setBounds] = useState(null);
-    const [centerLat, setCenterLat] = useState(null);
-    const [centerLng, setCenterLng] = useState(null);
-    const [activityCoords, setActivityCoords] = useState([]);
-
-    console.log(city, 'city')
-    console.log(activities, 'act')
-
-    const generateActivityCoords = (place, id) => {
-        Geocode.fromAddress(place).then(
-            (response) => {
-                const {lat, lng } = response.results[0].geometry.location;
-                let obj = { id: id, lat: lat, lng: lng, title: place }
-                setActivityCoords(old => [...old, obj])
-            },
-            (error) => {
-                console.error(error);
-            }
-        );
-    };
-
-    useEffect(() => {
-        const generateCityLatLng = async (city) => {
-                const res = await Geocode.fromAddress(city);
-                const { lat, lng } = res.results[0].geometry.location;
-                setCenterLat(lat);
-                setCenterLng(lng);
-                setMapLoaded(true);
-            
-        };
-        generateCityLatLng();
-    });
 
 
 
 
-    useEffect(() => {
-            activities.forEach((activity) => {
-                generateActivityCoords(activity.title, activity._id);
-            });  
-    }, [activities]);
 
-    
-
-    //   useEffect(() => {
-    //     if (activities && activityCoords.length > 0) {
-    //       setMapLoaded(true);
-    //     } else if (!activities) {
-    //         setMapLoaded(true);
-    //     }
-    //   }, [activityCoords]);
-
-    //   const mapEventHandlers  = useMemo(() => ({
-    //       click: event => {
-    //         const search = new URLSearchParams(event.latLng.toJSON()).toString();
-    //         history.push({ pathname: '/trip/:tripID', search });
-    //         },
-    //         idle: map => setBounds(map.getBounds().toUrlValue())
-    //   }), [history]);
-
-
-
-    if (mapLoaded) return (
+    return (
         <>
             <div className='act-map-container'>
-                { centerLat&& <ActivitiesMap  
-                centerLat={centerLat}
-                centerLng={centerLng}
-                activities={activityCoords}
+                && <ActivitiesMap  
+                // cityLat={}
+                // cityLng={centerLng}
+                // activities={activityCoords}
                 // mapEventHandlers={mapEventHandlers}
                 markerEventHandlers={{
                     click: (activity) => history.push(`//${activity._id}`),
@@ -85,7 +27,7 @@ function TripMap ({city, activities}) {
                     mouseout: () => setHighlightedActivity(null)
                 }}
                 highlightedActivity={highlightedActivity}
-                />}
+                />
             </div>
         </>
     )
