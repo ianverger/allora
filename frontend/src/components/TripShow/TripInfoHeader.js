fcb9ecb9ec8df789e41666ddee1032b4f86c0a2d
heatherpark201 <heatherpark201@gmail.com>
2022-11-28 11:30:43 -0500

    update tripinfoheader with attendees

--- frontend/src/components/TripShow/TripInfoHeader.js
+++ frontend/src/components/TripShow/TripInfoHeader.js
@@ -10,23 +10,25 @@ function TripInfoHeader ({ city, dates, title, attendees }) {
             <div id='trip-image'> 
                 <img id='trip-img' src={'https://hippark-photos.s3.amazonaws.com/allora-pics/veliko-karachiviev-hSvagWirWPA-unsplash.jpg'} alt=""></img>
             </div>
-            <header id='trip-title-wrapper'>
-                <span>{title}</span>
-            </header>
-            <div id='under-title-wrapper'>
-                <span>{dates.length} Days in {city}</span>
-            </div>
-             <div id='attendees-spotlight-container'>
-                {attendees.map((attendee, idx) => (
-                    <div id='attendee-icon-each' key={idx}>
-                        <AttendeeIcon
-                            attendee={attendee}
-                        />
-                    </div>
-                ))}
-            </div>
-            <div id='trip-dates-container'>
-                <span>{dates.at(0)} - {dates.at(dates.length-1)}</span>
+            <div id='under-image-container'>
+                <header id='trip-title-wrapper'>
+                    <div id='trip-title'>{title}</div>
+                </header>
+                <div id='under-title-wrapper'>
+                    <span>{dates.length} Days in {city}</span>
+                </div>
+                <div id='attendees-spotlight-container'>
+                    {attendees.map((attendee, idx) => (
+                        <div id='attendee-icon-each' key={idx}>
+                            <AttendeeIcon
+                                attendee={attendee}
+                                />
+                        </div>
+                    ))}
+                </div>
+                <div id='trip-dates-container'>
+                    <span>{dates.at(0)} - {dates.at(dates.length-1)}</span>
+                </div>
             </div>
 
         </div>
